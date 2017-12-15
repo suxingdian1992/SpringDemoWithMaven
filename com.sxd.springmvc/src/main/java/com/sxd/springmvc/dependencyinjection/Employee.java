d0e7261bc9bc9eba8f49cf4409430db06efa4767
suxingdian1992 <suxingdian@live.com>
2017-12-15 09:41:23 +0800

    依赖注入实例，注意关注bean使用setter和使用构造函数实例化对象的方法

---
+++ com.sxd.springmvc/src/main/java/com/sxd/springmvc/dependencyinjection/Employee.java
@@ -0,0 +1,47 @@
+package com.sxd.springmvc.dependencyinjection;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private Address homeAddress;//setter注入，将adress类注入到employee中
+    
+    public Employee() {
+    }
+    
+    public Employee(String firstName, String lastName, Address homeAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.homeAddress = homeAddress;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName
+                + "\n" + homeAddress;
+    }
+
+}
