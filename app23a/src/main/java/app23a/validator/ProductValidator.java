f3ac3c7aa6696b8f1a4989e2a8d825438d67f535
suxingdian1992 <suxingdian@live.com>
2017-12-19 11:00:15 +0800

    app23a文件上传
    如何在spring中利用MultipartFile接口进行文件上传

---
+++ app23a/src/main/java/app23a/validator/ProductValidator.java
@@ -0,0 +1,23 @@
+package app23a.validator;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import app23a.domain.Product;
+
+public class ProductValidator implements Validator {
+
+    public boolean supports(Class<?> klass) {
+        return Product.class.isAssignableFrom(klass);
+    }
+
+    public void validate(Object target, Errors errors) {
+        Product product = (Product) target;
+        ValidationUtils.rejectIfEmpty(errors, "name", "productname.required");
+        Float price = product.getPrice();
+        if (price != null && price < 0) {
+            errors.rejectValue("price", "price.negative");
+        }
+    }
+}
